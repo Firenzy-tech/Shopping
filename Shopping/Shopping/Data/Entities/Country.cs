36b8578681dc67806269b6ec6f323d8e295fc92a
Firenzy-tech <81311111+Firenzy-tech@users.noreply.github.com>
2022-03-06 09:32:25 -0500

    Update Error Database

---
+++ Shopping/Shopping/Data/Entities/Country.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+
+        public int Id { get; set; }
+        [Display(Name = "País")]
+        [MaxLength(50, ErrorMessage = "El campó{0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage =    "El campo {0} es obligatorio. ")]
+
+
+        public int Name { get; set; }
+
+    }
+}
